f637c2abef3648c7ffa404f39bdff1048a0cfda0
Rohit Yadav <developer.royad@gmail.com>
2023-08-24 17:00:06 +0530

    fixing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,11 @@ const Navbar = () => {
     return (
         <nav className="bg-white border-gray-200 shadow sticky top-0 py-2 z-[100]">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center">
+                <a href="/" className="flex items-center">
                     <span className="self-center text-2xl font-semibold whitespace-nowrap">Rohit Yadav</span>
                 </a>
                 <div className="flex md:order-2">
-                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium duration-300  rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Hire Me</button>
+                    <button type='button' className="items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 duration-300 text-center">Hire Me</button>
                     <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-expanded={isMenuOpen ? 'true' : 'false'}>
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
